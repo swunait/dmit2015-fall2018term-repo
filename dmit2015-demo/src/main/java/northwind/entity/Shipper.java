476e1bc77007401945725ef7f3d87d08eec13032
Sam Wu <swu@nait.ca>
2018-10-11 16:44:45 -0600

    Updated Shipper.java to use custom class-level annotation

--- dmit2015-demo/src/main/java/northwind/entity/Shipper.java
+++ dmit2015-demo/src/main/java/northwind/entity/Shipper.java
@@ -5,6 +5,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import northwind.service.ValidCanadaPhone;
+
 import java.util.List;
 
 
@@ -12,6 +14,7 @@ import java.util.List;
  * The persistent class for the Shippers database table.
  * 
  */
+@ValidCanadaPhone
 @Entity
 @Table(name="Shippers")
 @NamedQuery(name="Shipper.findAll", query="SELECT s FROM Shipper s")
