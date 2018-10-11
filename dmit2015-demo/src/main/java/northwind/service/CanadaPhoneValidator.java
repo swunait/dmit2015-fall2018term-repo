a3151ec466f2140a4696b0e746f0d519ab6c405b
Sam Wu <swu@nait.ca>
2018-10-11 16:33:51 -0600

    Added custom class-level annotation example

---
+++ dmit2015-demo/src/main/java/northwind/service/CanadaPhoneValidator.java
@@ -0,0 +1,18 @@
+package northwind.service;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import northwind.entity.Shipper;
+
+public class CanadaPhoneValidator implements ConstraintValidator<ValidCanadaPhone, Shipper>{
+
+	@Override
+	public boolean isValid(Shipper currentShipper, ConstraintValidatorContext context) {
+		if (currentShipper == null) {
+			return true;
+		}
+		return currentShipper.getPhone().matches("^(\\([0-9]{3}\\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}");
+	}
+
+}
